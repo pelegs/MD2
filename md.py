865c0a81f67bdface58ef0ba6afebea1817d1dcb
Peleg Sapir <pelegs@gmail.com>
2021-10-12 20:04:56 +0200

    Added cell class

--- md.py
+++ md.py
@@ -6,7 +6,7 @@ from numpy.linalg import norm
 from itertools import product
 
 
-# -------- General maths functions  ------- #
+# ----------------- General maths functions  ----------------- #
 
 def wrap(x, L):
     """
@@ -17,23 +17,15 @@ def wrap(x, L):
     """
     return x-L*np.floor(x/L)
 
-def get_neighboring_indices(indices, n=10, step=1, dim=3):
-    if dim==2:
-        i, j = indices
-    elif dim==3:
-        i, j, k = indices
-    else:
-        raise ValueError('Dim must be 2 or 3.')
+def get_neighboring_indices(indices, n=10, step=1):
+    i, j, k = indices
     x_ind = [x%n for x in np.arange(i-step, i+step+1, 1)]
     y_ind = [y%n for y in np.arange(j-step, j+step+1, 1)]
-    if dim==2:
-        return list(product(x_ind, y_ind))
-    elif dim==3:
-        z_ind = [z%n for z in np.arange(k-step, k+step+1, 1)]
-        return list(product(x_ind, y_ind, z_ind))
+    z_ind = [z%n for z in np.arange(k-step, k+step+1, 1)]
+    return list(product(x_ind, y_ind, z_ind))
 
 
-# -------- Vector related functions ------- #
+# ----------------- Vector related functions ----------------- #
 
 def norm_sqr(v):
     return np.sum([x**2 for x in v])
@@ -45,7 +37,7 @@ def dist(v1, v2):
     return norm(v1-v2)
 
 
-# -------- Atom class  ------- #
+# ----------------- Atom class  ----------------- #
 
 class Atom:
     def __init__(self,
@@ -91,7 +83,37 @@ class Atom:
         self.KE = 0.5 * self.m * norm_sqr(self.vel)
 
 
-# -------- Grid class  ------- #
+# ----------------- Cell class  ----------------- #
+
+class Cell:
+    def __init__(self, index1d, index3d, ul_corner, L=np.ones(3)):
+        self.index1d = index1d
+        self.index3d = index3d
+        self.ul_corner = ul_corner
+        self.L = L
+        self.neighbors = []
+        self.particles = []
+
+    def clear(self):
+        self.particles.clear()
+
+    def insert(self, obj):
+        self.particles.append(obj)
+
+    def remove(self, obj):
+        if obj in self.particles:
+            self.particles.remove(obj)
+
+    def set_neighbors(self, neighbor_cells):
+        self.neighbors = neighbor_cells
+
+    def __str__(self):
+        return 'idx1d: {}, idx3d: {}, corners: {}'.format(
+            self.index1d, self.index3d, self.corners
+        )
+
+
+# ----------------- Grid class  ----------------- #
 
 class Grid:
     def __init__(self, n=10, L=np.ones(3)):
@@ -99,17 +121,30 @@ class Grid:
         self.L = np.array(L)
         self.cell_dim = self.L/n
         self.bins = np.linspace((0,0,0), self.L, self.n+1).T
-        self.reset()
-        self.indices = [(x,y) for x in range(n) for y in range(n)]
-
-    def reset(self):
-        self.cells = [[[[]
-                      for _ in range(self.n)]
-                      for _ in range(self.n)]
-                      for _ in range(self.n)
+        self.indices = [
+            x for x in product(range(n), range(n), range(n))
         ]
 
-    def insert(obj, index=np.zeros(3)):
+        # Create cells
+        self.cells = []
+        for index1d, index3d in enumerate(self.indices):
+            corners = self.get_cell_ul_corner(index3d)
+            new_cell = Cell(index1d, index3d, corners, self.L/self.n)
+            self.cells.append(new_cell)
+
+        # Set neighbors for each cell
+        for cell in self.cells:
+            neighbors = [self.cells[self.get_index_1d_from_3d(index3d)]
+                         for index3d in get_neighboring_indices(
+                            cell.index3d, self.n, step=2
+                        )]
+            cell.set_neighbors(neighbors)
+
+    def reset(self):
+        for cell in self.cells:
+            cell.reset()
+
+    def insert(self, obj):
         # Check validity of index
         for i in range(3):
             if not (0 <= index[i] < self.n):
@@ -117,28 +152,36 @@ class Grid:
                 return
 
         # Add obj to cell
-        ix, iy, iz = self.get_indices(obj.pos)
-        self.cells[ix][iy][iz] = obj
+        index3d = self.get_index_3d_from_pos(obj.pos)
+        index1d = self.get_index_1d_from_3d(index3d)
+        self.cells[index1d].insert(obj)
 
-    def get_indices(self, pos):
+    def get_index_3d_from_pos(self, pos):
         """
         Returns the cell index for the position `pos`.
         NOTE: periodic boundry conditions.
         """
         pos_wrapped = [wrap(x, L) for x, L in zip(pos, self.L)]
-        indices = [np.digitize(x, b) for x, b in zip(pos_wrapped, self.bins)]
+        index = [np.digitize(x, b) for x, b in zip(pos_wrapped, self.bins)]
         # subtract 1 from each index because np starts counting from 1
-        indices = [n-1 for n in indices]
-        return indices
+        index = [n-1 for n in index]
+        return index
 
-    def get_coordinates(self, index):
-        i, j = index
-        c1 = (self.bins[0,i], self.bins[1,j])
-        c2 = (self.bins[0,i+1], self.bins[1,j+1])
-        return np.array([c1, c2])
+    def get_index_3d_from_1d(self, idx1d):
+        return self.indices[idx1d]
+
+    def get_index_1d_from_3d(self, idx3d):
+        i, j, k = idx3d
+        return i*self.n**2 + j*self.n + k - 1
+
+    def get_cell_ul_corner(self, index3d):
+        i, j, k = index3d
+        return np.array([self.bins[0,i], self.bins[1,j], self.bins[2,k]])
 
 
-# -------- Main  ------- #
+# ----------------- Main  ----------------- #
 
 if __name__ == '__main__':
-    grid = Grid(n=10, L=[800,600,0])
+    grid = Grid(n=10, L=[800,600,400])
+    for cell in grid.cells[0].neighbors:
+        print(cell.index3d)
--- test.py
+++ test.py
@@ -8,16 +8,15 @@ import pygame
 from pygame.locals import *
 
 
-def draw_cell(surface, grid, index, fill=[255,0,0]):
-    ulc = grid.get_coordinates(index)[0]
-    dims = grid.L/grid.n
-    rect = (ulc[0], ulc[1], dims[0], dims[1])
+def draw_cell(surface, cell, fill=[0,0,0]):
+    rect = (cell.ul_corner[0], cell.ul_corner[1],
+            cell.L[0], cell.L[1])
     pygame.draw.rect(surface, fill, rect)
     pygame.draw.rect(surface, [255, 255, 255], rect, width=2)
 
 
 width, height = 800, 800
-grid = Grid(n=15, L=[width,height,0])
+grid = Grid(n=20, L=[width,height,0])
 
 pygame.init()
 
@@ -26,6 +25,10 @@ fpsClock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((width, height))
 
+for index1d, cell in enumerate(grid.cells):
+    draw_cell(screen, cell, [0,0,0])
+cursor_idx_1d = 0
+current_cell = grid.cells[0]
 
 # Game loop
 while True:
@@ -35,19 +38,23 @@ while True:
             sys.exit()
 
     # Mechanism
+    cursor_idx_1d_prev = cursor_idx_1d
+    prev_cell = grid.cells[cursor_idx_1d_prev]
     cursor_pos = pygame.mouse.get_pos()
-    cursor_idx = tuple(grid.get_indices(cursor_pos))
-    neighbor_idx = get_neighboring_indices(cursor_idx, grid.n, 1, dim=2)
+    cursor_pos_3d = (cursor_pos[0], cursor_pos[1], 0)
+    cursor_idx_3d = tuple(grid.get_index_3d_from_pos(cursor_pos_3d))
+    cursor_idx_1d = grid.get_index_1d_from_3d(cursor_idx_3d)
+    current_cell = grid.cells[cursor_idx_1d]
 
     # Draw
-    for idx in grid.indices:
-        if idx in neighbor_idx:
-            if tuple(idx) == tuple(cursor_idx):
-                draw_cell(screen, grid, idx, [0,255,0])
-            else:
-                draw_cell(screen, grid, idx, [0,150,0])
-        else:
-            draw_cell(screen, grid, idx, [0,0,0])
+    if cursor_idx_1d != cursor_idx_1d_prev:
+        draw_cell(screen, prev_cell, [0,0,0])
+        prev_cell = current_cell
+        for neighbor in grid.cells[cursor_idx_1d_prev].neighbors:
+            draw_cell(screen, neighbor, [0,0,0])
+    for neighbor in grid.cells[cursor_idx_1d].neighbors:
+        draw_cell(screen, neighbor, [0,150,0])
+    draw_cell(screen, current_cell, [0,255,0])
     pygame.draw.circle(screen, [255,0,0], cursor_pos, 3)
 
     # Update
